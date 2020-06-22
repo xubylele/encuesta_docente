1b2e8c9cccd89e86e759c352c1dadc95c9d5eb35
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-22 19:55:19 -0400

    FIX UserController COMMENTS

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,11 +5,12 @@ const userCtrl = {};
 // ***** BASICAS ****
 userCtrl.createUser= async (req, res) => {
 
-    const user = await new userModel(req,res);      //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
-    /*FALTA FUNCIÓN IF EXISTE EN BASE DE DATOS */   
-    await user.save();                              //GUARDAMOS
+    const user = await new userModel(req,res);                 //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
+    /*FALTA FUNCIÓN IF EXISTE
+     EN BASE DE DATOS */   
+    await user.save();                                         //GUARDAMOS
     res.json({
-        'status': 'User Created'                    //DEVOLVEMNOS EL ESTADO DE CREADO
+        'status': 'User Created'                               //DEVOLVEMNOS EL ESTADO DE CREADO
     });
 }
 
@@ -19,22 +20,23 @@ userCtrl.getUser= async (req, res) => {
 }
 
 userCtrl.editUser = async (req, res) => {
-    const { id } = req.params;
-    const user = {
+    const { id } = req.params;                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
+    const user = {                                              // PARAMETROS A EDITAR
     //   name: req.body.name,
     //    last_name: req.body.last_nameM,
-        email: req.body.email,
+        email: req.body.email,                  
         password: req.body.password,
         timestap: req.body.timestap
-        /* PARA AGREGAR ENCUESTAS Y PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
+        /* PARA AGREGAR ENCUESTAS Y 
+        PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
     }
-    await userModel.findByIdAndUpdate(id,{$set: user});
+    await userModel.findByIdAndUpdate(id,{$set: user});         // BUSCAMOS Y GUARDAMOS
 }
 
 userCtrl.remove = async(req, res) => {
-    const user = userModel.findByIdAndRemove(req.params.id);
-    res.json({
-        'status': 'User Deleted'
+    const user = userModel.findByIdAndRemove(req.params.id);    // BUSCAMOS Y ELIMINAMOS
+    res.json({                              
+        'status': 'User Deleted'                                //LE MANDAMOS EL ESTADO ACTUALIZADO
     });
 }
 
