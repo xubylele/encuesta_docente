18d3557662f9254e8d7b39dd7749c6fd782fad34
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-23 17:01:28 -0400

    update userEnsignController

--- server/src/controllers/UserEnsignController.js
+++ server/src/controllers/UserEnsignController.js
@@ -1,7 +1,21 @@
 const userEnsignsModel = require('../models/UserEnsigns');
+
 const userEnsignsCtrl = {}
 
 
-userEnsignsCtrl.createUserEnsign = async (req, res) {
+userEnsignsCtrl.createUserEnsigns = async (req, res) => {
+const createUserEnsigns = await new userEnsignsModel(req, res);
+await userEnsignsModel.save();
+res.json({
+    'status': 'User Ensigns Created'
+});
+}
 
+userEnsignsCtrl.getUserEnsigns = async(req, res) => {
+    const userEnsigns = await userEnsignsModel.findById(req.params.id);
+    res.json(userEnsigns)
+}
+
+userEnsignsCtrl.remove = async(req, res) => {
+    userEnsignsModel.findByIdAndDelete(req.params.id);
 }
