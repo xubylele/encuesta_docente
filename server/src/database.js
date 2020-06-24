00db30d42e6fba80393473d6720a380e4c40616b
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-23 22:58:42 -0400

    ADDED DATABASE URL

--- server/src/database.js
+++ server/src/database.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true
-})
+const db_connection = 'mongodb://companiereserver.systems:25545'; // DEVELOPER EXTERNAL
+//const db_connection = 'mongodb://172.0.0.x:27017';                    // DOCKER INTERNAL
+mongoose.connect(db_connection)
     .then(db => console.log('DB connected successfully'))
     .catch(err => console.error(err))
