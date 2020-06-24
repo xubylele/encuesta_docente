e341fafc8fbf2a769237ccdebe2a39bd47c7e8de
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-23 23:03:38 -0400

    Revert "ADDED DATABASE URL"
    This reverts commit 00db30d42e6fba80393473d6720a380e4c40616b.

--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const db_connection = 'mongodb://companiereserver.systems:25545'; // DEVELOPER EXTERNAL
-//const db_connection = 'mongodb://172.0.0.x:27017';                    // DOCKER INTERNAL
-mongoose.connect(db_connection)
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true
+})
     .then(db => console.log('DB connected successfully'))
     .catch(err => console.error(err))
