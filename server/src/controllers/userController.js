7200e6fef16dcff3ea51eeed5ed907c8e10d193c
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-29 00:20:17 -0400

    added try and catch to user controller

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,31 +5,39 @@ const userCtrl = {};
 
 // ***** BASICAS ****
 userCtrl.createUser= async (req, res) => {
-
+try {
+    
     const user = await new userModel(req,res);                 //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
     /*FALTA FUNCIÓN IF EXISTE
      EN BASE DE DATOS */   
     await user.save();                                         //GUARDAMOS
-    res.json({
-        'status': 'User Created'                               //DEVOLVEMNOS EL ESTADO DE CREADO
-    });
+    res.status(200).json({user: user, message: 'Usuario creado con exito'});
+} catch (error) {
+    res.status(500).json({error});
+}
 }
 
 userCtrl.getUsers = async (req, res) =>{
+try {
     const users = await userModel.find();
     res.status(200).json(users);
+} catch (error) {
+    res.status(500).json({error});
+}
 }
 
 userCtrl.getUser= async (req, res) => {
+try {
     const user = await userModel.findById(req.params.id);
-    res.json(user);
-}
+    res.status(200).json(user);
+} catch (error) {
+    res.status(500).json({error});
+}}
 
 userCtrl.editUser = async (req, res) => {
+try {
     const { id } = req.params;                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
     const user = {                                              // PARAMETROS A EDITAR
-    //   name: req.body.name,
-    //    last_name: req.body.last_nameM,
         email: req.body.email,                  
         password: req.body.password,
         timestap: req.body.timestap
@@ -37,6 +45,10 @@ userCtrl.editUser = async (req, res) => {
         PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
     }
     await userModel.findByIdAndUpdate(id,{$set: user});         // BUSCAMOS Y GUARDAMOS
+    res.status(200).json({user: user, message: 'Usuario editado con exito'})
+} catch (error) {
+    res.status(500).json({error})
+}
 }
 
 userCtrl.remove = async(req, res) => {
