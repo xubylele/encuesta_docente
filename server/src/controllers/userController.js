3c1f05c2a7ee17daab5f2ba437401dc92a0b84a8
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-29 01:30:56 -0400

    fix userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,10 +4,11 @@ const userCtrl = {};
 
 
 // ***** BASICAS ****
-userCtrl.createUser= async (req, res) => {
+userCtrl.createUser = async (req, res) => {
+const user = new userModel(req.body);                 //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
 try {
-    
-    const user = await new userModel(req,res);                 //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
+    console.log(user);
+
     /*FALTA FUNCIÓN IF EXISTE
      EN BASE DE DATOS */   
     await user.save();                                         //GUARDAMOS
@@ -35,21 +36,20 @@ try {
 }}
 
 userCtrl.editUser = async (req, res) => {
-try {
-    const { id } = req.params;                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
-    const user = {                                              // PARAMETROS A EDITAR
-        email: req.body.email,                  
-        password: req.body.password,
-        timestap: req.body.timestap
-        /* PARA AGREGAR ENCUESTAS Y 
-        PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
+    try {
+        const { id } = req.params;                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
+        const user = {                                              // PARAMETROS A EDITAR
+            email: req.body.email,                  
+            password: req.body.password
+            /* PARA AGREGAR ENCUESTAS Y 
+            PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
+        }
+        await userModel.findByIdAndUpdate(id,{$set: user});         // BUSCAMOS Y GUARDAMOS
+        res.status(200).json({user: user, message: 'Usuario editado con exito'})
+    } catch (error) {
+        res.status(500).json({error})
     }
-    await userModel.findByIdAndUpdate(id,{$set: user});         // BUSCAMOS Y GUARDAMOS
-    res.status(200).json({user: user, message: 'Usuario editado con exito'})
-} catch (error) {
-    res.status(500).json({error})
-}
-}
+}/* OJO QUE LA FUNCIÓN AL CAMBIAR LA PASSWORD NO LA ENCRIPTA. AVERIGUAR */
 
 userCtrl.remove = async(req, res) => {
     console.log('gola')
