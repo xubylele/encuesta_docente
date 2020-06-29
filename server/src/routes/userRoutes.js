b174ed08d320ce89c8ab4361ff339419e7f3921c
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-29 00:54:23 -0400

    edit user routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-    router.get('/api/user/getusers', userController.getUsers);
-    router.get('/api/user/:id/get',userController.getUser);
-    router.post('/api/user/create',UserController.createUser);
-    router.delete('/api/user/delete', userController.remove);
-    router.put('/api/user/:id/edit',userController.editUser);
+    router.get('/getusers', userController.getUsers);
+    router.get('/:id/get',userController.getUser);
+    router.post('/create',UserController.createUser);
+    router.delete('/delete', userController.remove);
+    router.put('/:id/edit',userController.editUser);
 
     module.exports = router;
