6eb68a9957b49082ee919f42546b27fe0b8bf2fb
Oliver Sánchez <elxoliver29@gmail.com>
2020-06-29 01:51:33 -0400

    user controller basic functions redy and tested

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,13 +5,13 @@ const userCtrl = {};
 
 // ***** BASICAS ****
 userCtrl.createUser = async (req, res) => {
-const user = new userModel(req.body);                 //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
+const user = new userModel(req.body);                                             //LE ASIGNAMOS LOS DATOS QUE INGRESAN EN EL FRONT
 try {
     console.log(user);
-
+    
     /*FALTA FUNCIÓN IF EXISTE
      EN BASE DE DATOS */   
-    await user.save();                                         //GUARDAMOS
+    await user.save();                                                            //GUARDAMOS
     res.status(200).json({user: user, message: 'Usuario creado con exito'});
 } catch (error) {
     res.status(500).json({error});
@@ -37,14 +37,14 @@ try {
 
 userCtrl.editUser = async (req, res) => {
     try {
-        const { id } = req.params;                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
-        const user = {                                              // PARAMETROS A EDITAR
+        const { id } = req.params;                                                  // DEJAMOS EL ID , TONMANDOLOS DESDE LO ANTERIOR
+        const user = {                                                              // PARAMETROS A EDITAR
             email: req.body.email,                  
             password: req.body.password
             /* PARA AGREGAR ENCUESTAS Y 
             PARTICIPANTES HARÉMOS OTRA FUNCIÓN */
         }
-        await userModel.findByIdAndUpdate(id,{$set: user});         // BUSCAMOS Y GUARDAMOS
+        await userModel.findByIdAndUpdate(id,{$set: user});                         // BUSCAMOS Y GUARDAMOS
         res.status(200).json({user: user, message: 'Usuario editado con exito'})
     } catch (error) {
         res.status(500).json({error})
@@ -54,7 +54,7 @@ userCtrl.editUser = async (req, res) => {
 userCtrl.remove = async(req, res) => {
     console.log('gola')
     try {
-        const user = await userModel.findByIdAndRemove(req.body.id);    // BUSCAMOS Y ELIMINAMOS
+        const user = await userModel.findByIdAndRemove(req.body.id);                // BUSCAMOS Y ELIMINAMOS
         res.status(200).json({user: user, message: 'Ususario eliminado con exito'})
         
     } catch (error) {
