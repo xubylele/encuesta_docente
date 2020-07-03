10c3809282c9c9908848dbc6d8e8a569a172d8d8
Oliver Sánchez <elxoliver29@gmail.com>
2020-07-03 00:58:37 -0400

    fix poll controller &add poll router

---
+++ server_buenardo/src/routes/Pollrouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.router();
+//const { PollController } = require('../controllers');
+const PollController = require('../controllers/PollController');
+    
+    router.post('/create', PollController.create);
+    router.get('/getallcontroller',PollController.getAllpolls);
+    router.get('/:id/get',PollController.getpoll);
+    router.delete('/delete',PollController.deletepoll);
+    router.put('/addanswer',PollController.addAnswer);
+
+    module.exports = router;
