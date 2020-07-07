97906a5338b5ae354add290c594f76209e433c19
Manuel <mifi.marin@gmail.com>
2020-07-07 04:04:46 -0400

    Import CHART.JS. Works On Insignias

--- client/src/app/profesor/insignias/insignias.component.ts
+++ client/src/app/profesor/insignias/insignias.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
+
 
 @Component({
   selector: 'app-insignias',
@@ -7,13 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsigniasComponent implements OnInit {
 
-
-
-    constructor() {
-        
-    }
-
+  canvas: any;
+  ctx: any;
+  constructor() { }
   ngOnInit(): void {
-  }
+  
+  this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    let myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+          labels: ["USA", "Spain", "Italy", "France", "Germany", "UK", "Turkey", "Iran", "China", "Russia", "Brazil", "Belgium", "Canada", "Netherlands", "Switzerland", "India", "Portugal", "Peru", "Ireland", "Sweden"],
+          datasets: [{
+              label: 'Total cases.',
+              data: [886789, 213024, 189973, 158183, 153129, 138078, 101790, 87026, 82804, 62773, 50036, 42797, 42110, 35729, 28496, 23502, 22353, 20914, 17607, 16755],
+              backgroundColor: ["red", , , , , , , , "orange"],
+              borderWidth: 1
+          }]
+      },
+      options: {
+    legend: {
+        display: false
+    },
+        responsive: false,
+        display:true
+      }
+    });
+  
+  
 
+  }
 }
