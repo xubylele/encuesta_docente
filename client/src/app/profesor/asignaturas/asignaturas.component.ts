1aa5fc0ad3df982e7403603735d7e0507bff6bde
Oliver Sánchez <elxoliver29@gmail.com>
2020-07-07 17:30:14 -0400

    addd plain data to component, added cards to html and sccs in asignaturas

--- client/src/app/profesor/asignaturas/asignaturas.component.ts
+++ client/src/app/profesor/asignaturas/asignaturas.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsignaturasComponent implements OnInit {
 
+  courseList: Array<any>;
+
   constructor() { }
 
+
+
   ngOnInit(): void {
+    this.courseList = [{name:"first",acronym:"INF-12", participantsCount: 30},
+                       {name:"second",acronym:"INF-13", participantsCount: 20},
+                       {name:"thirth",acronym:"INF-14", participantsCount: 220},
+                       {name:"fourty",acronym:"INF-15", participantsCount: 35}];
   }
 
 }
