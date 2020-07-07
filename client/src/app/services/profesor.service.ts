1f2fc7a864de0bb7dff920e71fb441e42ac2734a
Oliver Sánchez <elxoliver29@gmail.com>
2020-07-07 17:52:55 -0400

    added many hosts vars to profesor service

--- client/src/app/services/profesor.service.ts
+++ client/src/app/services/profesor.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject} from 'rxjs';
+import { sectionList } from './../models/encuesta'
+import { RespuestaI } from '../models/respuesta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesorService {
-
+  HOST:string = 'http://localhost:300/api/';
+  HOST_POLLS:string = `${this.HOST}polls/`;
+  HOST_TEACHERSBADGES:string = `${this.HOST}teachersbadges/`;
+  HOST_PARTICIPANTLIST:string = `${this.HOST}participantlist/`;
+  HOST_USERS:string = `${this.HOST}users/`;
   constructor() { }
 }
