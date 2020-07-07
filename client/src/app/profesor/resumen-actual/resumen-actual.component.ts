1b5bde4448d0018392419c980f9e2fcdf8176400
Oliver Sánchez <elxoliver29@gmail.com>
2020-07-07 14:59:14 -0400

    some commentaries

--- client/src/app/profesor/resumen-actual/resumen-actual.component.ts
+++ client/src/app/profesor/resumen-actual/resumen-actual.component.ts
@@ -17,58 +17,54 @@ export class ResumenActualComponent implements OnInit {
   chartData: any;
 
   ngOnInit(): void {
-    this.setData();
-    this.canvas = document.getElementById('resumeChart');
-    this.ctx = this.canvas.getContext('2d');
-    let myChart = new Chart(this.ctx, {
-      type: 'polarArea',
-      ticks:{
-          sampleSize: 5,
-          max: 5
-      },
-      data: {
-          labels: this.chartLabels,
-          datasets: [{
-              fill: 2,
-              data: this.chartData,
-              backgroundColor: ["rgba(255, 99, 132, 0.8)","rgba(75, 192, 192, 0.8)","rgba(255, 205, 86, 0.8)","rgba(201, 203, 207, 0.8)","rgba(54, 162, 235, 0.8)"  ],
-              borderWidth: 1
+    this.setData();                                                 // SETEAMOS LA DATA
+    this.canvas = document.getElementById('resumeChart');           // OBTENEMOS EL ELEMENTO POR ID
+    this.ctx = this.canvas.getContext('2d');                        // LE DAMOS CONTEXTO DE 2 DIMENSIONES
+    let resumeChart = new Chart(this.ctx, {                         // CREAMOS EL CHART             
+      type: 'polarArea',                                            // LE ASIGNAMOS EL TIPO
+      data: {                                                       // SETEAMOS LA DATA DEL CHART
+          labels: this.chartLabels,                                 // ASIGNAMOS LOS LABELS A LOS GLOBALES
+          datasets: [{                                              // DECLARAMOS EL DATA SET
+              data: this.chartData,                                 // ASIGNAMOS LA DATA A LA GLOBAL
+              backgroundColor: ["rgba(255, 99, 132, 0.8)",          // COLOR LABEL 1
+                                "rgba(75, 192, 192, 0.8)",          // COLOR LABEL 2
+                                "rgba(255, 205, 86, 0.8)",          // COLOR LABEL 3
+                                "rgba(201, 203, 207, 0.8)",         // COLOR LABEL 3
+                                "rgba(54, 162, 235, 0.8)"],         // COLOR LABEL 5
+              borderWidth: 1                                        // LE ASIGNAMOS EL ANCHO DEL BORDE
           }]
       },
-      options: {
-        scale: {
-          ticks: {
-              min: 0,
-              max: 4
+      options: {                                                    // SETEAMOS LAS OPCIONES
+        scale: {                                                    // DECLARAMOS LA ESCALA
+          ticks: {                          
+              min: 0,                                               // TIENE MINIMO 0
+              max: 4                                                // MAXIMO 4
           }
         },
-    legend: {
-        display: true
-    },
-        responsive: false,
-        display:true
+        legend: {
+            display: true                                           // ASIGNAMOS TRUE PARA QUE SE VEAN LAS LABELS 
+        },
+        responsive: false,                                          // TRABAJAMOS SIN RESPONSIVE
+        display:true                                                // LE DECIMOS QUE SI SE MUESTRE
       }
     });
   }
-
-
-
   setData():void{
-    //const resData = // obtenemos desde back
+    //const resData =                                                // OBTENEMOS LA DATA DESDE EL BACK 
     /* 
-    let data;                                 // VARIABLE TEMPORAL DATA
-    let labels;                               // VARIABLE TEMPORAL LABELS
+    let data;                                                        // VARIABLE TEMPORAL DATA
+    let labels;                                                      // VARIABLE TEMPORAL LABELS
 
-    for(let i = 0; i < resData.length;i++){   // FOR EACH 
-        labels[i] = resData[i].name[i];       // ASIGNAMOS EL NOMBRE DE LABEL POR CADA ITERACIÓN
-        data[i] = resData[i].puntuacion;      // ASIGNAMOS LA PUNTUACIÓN DE DATA POR CADA ITERACIÓN
-    }                                         // COMO AMBAS TIENEN EL MISMO INDICE SE PUEDE OCUPAR DE PANA
-    if(data!=null && labels != null){         // VERIFICAMOS QUE LA DATA NO SEA NULA
-    this.chartData = data;                    // LA SETEAMOS A LAS VARIABLES GLOBALES DE MAS ARRIBA
-    this.chartLabels = labels;                // X2
-    } else{                                   // SI NO 
-      this.chartData = null;                  // DEJAMOS EN NULL 
-      this.chartLabels = null;                // X2
+    for(let i = 0; i < resData.length;i++){                          // FOR EACH 
+        labels[i] = resData[i].name[i];                              // ASIGNAMOS EL NOMBRE DE LABEL POR CADA ITERACIÓN
+        data[i] = resData[i].puntuacion;                             // ASIGNAMOS LA PUNTUACIÓN DE DATA POR CADA ITERACIÓN
+    }                                                                // COMO AMBAS TIENEN EL MISMO INDICE SE PUEDE OCUPAR DE PANA
+    if(data!=null && labels != null){                                // VERIFICAMOS QUE LA DATA NO SEA NULA
+    this.chartData = data;                                           // LA SETEAMOS A LAS VARIABLES GLOBALES DE MAS ARRIBA
+    this.chartLabels = labels;                                       // X2
+    } else{                                                          // SI NO 
+      this.chartData = null;                                         // DEJAMOS EN NULL 
+      this.chartLabels = null;                                       // X2
     }
 
     
