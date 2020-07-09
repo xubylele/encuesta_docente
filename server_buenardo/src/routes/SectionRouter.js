65b23a7512424f6aa13c112d997e7877f7035619
Xavier Diaz <diazxavier27gmail.com>
2020-07-08 20:47:27 -0400

    Promedio por asignatura

--- server_buenardo/src/routes/SectionRouter.js
+++ server_buenardo/src/routes/SectionRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { SectionController } = require('../controllers');
+const auth = require('../middlewares/auth');
+const isTeacher = require('../middlewares/isTeacher')
 
     router.post('/create', SectionController.create)
     
@@ -8,5 +10,6 @@ const { SectionController } = require('../controllers');
     * Lo pides con un get simple, te retorno la lista, con el listado de preguntas dentro de cada seccion
     */
     router.get('/list', SectionController.getSectionList)
+    router.get('/averagePerSection', auth, isTeacher, SectionController.averagePerSection)
 
 module.exports = router;
