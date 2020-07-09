8889b37461cbb777ee675152578e8f443804573d
Xavier Diaz <diazxavier27gmail.com>
2020-07-09 01:29:52 -0400

    Grafico Fix

--- server_buenardo/src/controllers/SectionController.js
+++ server_buenardo/src/controllers/SectionController.js
@@ -41,13 +41,17 @@ sectionCtrl.averagePerSection = async(req , res) => {
                 if(poll != null)
                     for (let l = 0; l < poll.answers.length; l++) {
                         let answer = await Answer.findById(poll.answers[l])
-                        let question = await Question.findById(answer.question)
-                        pollCount++
+                        if(answer.question != null){
+                            console.log(answer.question)
+                            let question = await Question.findById(answer.question)
+                            pollCount++
+    
+                            if(question.section.toString() === section._id.toString()){
+                                let alternative = await Alternative.findById(answer.alternative) 
+                                sum = sum + alternative.alternative
+                                cont++
+                            }
 
-                        if(question.section.toString() === section._id.toString()){
-                            let alternative = await Alternative.findById(answer.alternative) 
-                            sum = sum + alternative.alternative
-                            cont++
                         }
                     }
             }
